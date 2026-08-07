2f2f0bffa125d7c8ac8530fbc212208c0c3564eb
glory <mgdwork12119241@gmail.com>
2026-08-07 04:41:01 +0300

    Initialize Supabase client

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,3 @@
 window.SUPABASE_URL='https://syphksuwfigpsvcsympa.supabase.co';
 window.SUPABASE_PUBLISHABLE_KEY='sb_publishable_pWcY5t5yt6PWO8Hj3hszMg_wrUqaTO9';
+window.supabaseClient=window.supabase.createClient(window.SUPABASE_URL,window.SUPABASE_PUBLISHABLE_KEY);
